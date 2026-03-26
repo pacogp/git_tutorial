56253a1f51e988963e62ccab19a4d13164929ccf
pacogp <paco.gomez@pedroortiz.com>
2026-03-26 18:19:32 +0100

    push 5 a remote

--- hello2.py
+++ hello2.py
@@ -2,4 +2,6 @@ print ("modificado22222")
 print ("2 push a git")
 print ("3 pull para actualizar a local")
 print ("4 pull para actualizar a local")
+print ("5 pull para actualizar a remote con push")
+
 
